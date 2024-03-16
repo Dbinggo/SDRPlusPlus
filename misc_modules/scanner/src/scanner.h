d3b227cfbbcf2d1685a82e687514bb4e7b56cdcb
YJDDB <djy1349678887@163.com>
2024-03-16 21:59:36 +0800

    add server usage

--- misc_modules/scanner/src/scanner.h
+++ misc_modules/scanner/src/scanner.h
@@ -1,11 +1,19 @@
-#pragma once;
-#include<string>
-class ScannerModule
-{
-private:
-    /* data */
-public:
-    ScannerModule(std::string name);
-    void HPstop(){}
-    void HPstart() {}
-};
+#ifndef HPSCANNER  
+#define HPSCANNER  
+  
+#include <string>  
+#include <module.h> // 确保这个头文件包含了ModuleManager::Instance的定义  
+  
+class ScannerModule : public ModuleManager::Instance {  
+public:  
+    ScannerModule(std::string name);  
+    void HPstop();
+    void HPstart();
+      
+  
+  
+private:  
+   
+};  
+  
+#endif // SCANNERMODULE_H
